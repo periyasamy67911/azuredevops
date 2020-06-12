7e7eaaecd9d22ac35b9203bc52e95022f3df70d3
Periyasamy Vellingirisamy <Periyasamy.Vellingirisamy@harman.com>
2020-06-12 13:34:08 +0530

    Return values changed in get action.

--- AzureDevOps/Controllers/ValuesController.cs
+++ AzureDevOps/Controllers/ValuesController.cs
@@ -1,8 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 namespace AzureDevOps.Controllers
 {
@@ -14,7 +11,7 @@ namespace AzureDevOps.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Periyasamy", "Vellingirisamy" };
         }
 
         // GET api/values/5
